a8646256ec6f3f6774628856a4da529b0b492c5c
Arsen0203 <117534181+Arsen0203@users.noreply.github.com>
2023-01-06 21:27:58 +0600

    Add files via upload

---
+++ DZ1_Arsen_22-01/main.js
@@ -0,0 +1,23 @@
+// let lang = prompt("Введите язык (ru или en):");
+//
+// if (lang === "ru") {
+//     console.log(['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']);
+// } else if (lang === "en") {
+//     console.log(['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']);
+// } else {
+//     alert("ERROR");
+// }
+
+
+// let lang = prompt("Введите язык (ru или en):");
+//
+// switch (lang) {
+//     case "ru":
+//         console.log(['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']);
+//         break;
+//     case "en":
+//         console.log(['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']);
+//         break;
+//     default:
+//         alert("ERROR");
+// }
